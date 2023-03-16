f84d0fa40cc1ef8a6da2d32cb9d2aba96c35f1f3
Hieu <emerald.hieu@gmail.com>
2023-03-17 02:01:52 +0700

    Implement Spirng Cloud Config Server and Client
    * cURLS to test:
    ** Server:
    *** curl http://root:s3cr3t@localhost:8888/awesome-app/dev
    *** curl http://root:s3cr3t@localhost:8888/awesome-app-dev.yml
    ** Client:
    *** curl http://localhost:8080/whoami/Hieu

---
+++ spring-cloud-config-server/src/main/java/com/emeraldhieu/cloud/ConfigServerApp.java
@@ -0,0 +1,14 @@
+package com.emeraldhieu.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApp {
+    
+    public static void main(String[] arguments) {
+        SpringApplication.run(ConfigServerApp.class, arguments);
+    }
+}
